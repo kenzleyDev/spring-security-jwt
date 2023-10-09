e635a66cc1d6928bab6774a80703744c280051a5
Luan Kenzley <kenzleydev@outlook.com>
2023-10-09 08:33:42 -0300

    criando entidade e repositório de usuários

---
+++ src/main/java/br/com/treinaweb/javajobs/models/User.java
@@ -0,0 +1,28 @@
+package br.com.treinaweb.javajobs.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+}
